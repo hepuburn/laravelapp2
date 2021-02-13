84f341293d97c0fb8d9f3b5274415a1ca8e216e5
hepuburn <cb400sbw9@yahoo.co.jp>
2021-02-13 16:41:51 +0900

    1

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -4,7 +4,21 @@
   <title>Index</title>
 </head>
 <body>
-  <h1>Hello/Index</h1>
-  <P>{{$msg}}</P>
+<h1>Hello/Index</h1>
+<p>{!!$msg!!}</p>
+<form action="/hello" method="get">
+  @csrf
+  <div>NAME:<input type="text" name="name" value="{{old('name')}}"></div>
+  <div>MAIL:<input type="text" name="mail" value="{{old('mail')}}"></div>
+  <div>TELL:<input type="text" name="tel" value="{{old('tel')}}"></div>
+  <input type="submit">
+</form>
+
+<hr>
+<ol>
+@for($i = 0;$i < count($keys);$i++)
+  <li>{{$keys[$i]}}:{{$values[$i]}}</li>
+@endfor
+</ol>
 </body>
 </html>
