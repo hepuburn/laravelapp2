754e839d382be3a80b9e4511b12f18a8e747d352
hepuburn <cb400sbw9@yahoo.co.jp>
2021-02-13 17:17:51 +0900

    コンストラクタ2-4

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Person;
 use Illuminate\Support\Facades\Storage;
+use App\MyClasses\MyService;
 
 
 class HelloController extends Controller
@@ -17,20 +18,13 @@ class HelloController extends Controller
        $this->fname = 'hello.txt';
     }
 
-    public function index(Request $request, Response $response)
+    public function index(MyService $myservice)
     {
-        $name = $request->query('name');
-        $mail = $request->query('mail');
-        $tel = $request->query('tel');
-        $msg = $request->query('msg');
-        $keys = ['名前', 'メール', '電話'];
-        $values = [$name, $mail, $tel];
         $data = [
-            'msg'=> $msg,
-            'keys'=> $keys,
-            'values'=> $values,
+            'msg'=> $myservice->say(),
+            'data'=> $myservice->data()
         ];
-        $request->flash();
+
         return view('hello.index', $data);
     }
 
